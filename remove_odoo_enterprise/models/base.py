16a073d86b5eb7a1804773ee880d281df3b359df
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2024-05-21 18:47:01 +0200

    [MIG] remove_odoo_enterprise: Migration to 17.0

--- remove_odoo_enterprise/models/base.py
+++ remove_odoo_enterprise/models/base.py
@@ -9,8 +9,8 @@ class Base(models.AbstractModel):
     _inherit = "base"
 
     @api.model
-    def search(self, domain, offset=0, limit=None, order=None, count=False):
-        res = super().search(domain, offset, limit, order, count)
-        if not count and self._name == "payment.provider":
+    def search(self, domain, offset=0, limit=None, order=None):
+        res = super().search(domain, offset, limit, order)
+        if self._name == "payment.provider":
             res = res.filtered(lambda a: not a.module_to_buy)
         return res
